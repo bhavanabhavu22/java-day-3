3176ff2fb155cf7810339782ba6d1b82f70afa32
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-04 12:29:24 +0530

    Add files via upload

---
+++ findingPOWER.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class findingPOWER
+{
+    public static void main(String args[])
+    {
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if((a&(a-1))==0)
+{
+    System.out.println("power of 2");
+}
+else
+{
+    System.out.println("not a power of 2");
+
+}
+    }
+}    
