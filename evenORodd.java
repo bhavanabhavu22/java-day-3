9f5744ad601491f63aba8ea912a1028d5af0c2cc
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-04 10:21:57 +0530

    Add files via upload

---
+++ evenORodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class evenORodd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+          if((n&1)==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
+    
